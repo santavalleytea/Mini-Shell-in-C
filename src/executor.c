43aeba597aa4b353a85a50b6caf410099678f33d
santavalleytea <takayaueno0712@gmail.com>
2025-07-31 15:31:58 +1000

    Implemented grep and >>

--- src/executor.c
+++ src/executor.c
@@ -22,6 +22,54 @@ void execute (char **argv) {
         exit(0);
     }
 
+    int pipe_pos = -1;
+    for (int i = 0; argv[i] != NULL; ++i) {
+        if (strcmp(argv[i], "|") == 0){
+            pipe_pos = i;
+            break;
+        }
+    }
+
+    if (pipe_pos != -1) {
+        argv[pipe_pos] = NULL;
+        char **left_argv = argv;
+        char **right_argv = &argv[pipe_pos + 1];
+
+        int pipefd[2];
+        if (pipe(pipefd) == -1) {
+            perror("Pipe");
+            exit(1);
+        }
+
+        pid_t pid1 = fork();
+        if (pid1 == 0) {
+            // Child 1; stdout to pipe
+            dup2(pipefd[1], STDOUT_FILENO);
+            close(pipefd[0]);
+            close(pipefd[1]);
+            execvp(left_argv[0], left_argv);
+            perror("execvp failed");
+            exit(1);
+        }
+
+        pid_t pid2 = fork();
+        if (pid2 == 0) {
+            // Child 2; stdin to pipe
+            dup2(pipefd[0], STDIN_FILENO);
+            close(pipefd[1]);
+            close(pipefd[0]);
+            execvp(right_argv[0], right_argv);
+            perror("execvp failed");
+            exit(1);
+        }
+
+        close(pipefd[0]);
+        close(pipefd[1]);
+        waitpid(pid1, NULL, 0);
+        waitpid(pid2, NULL, 0);
+        return;
+    }
+
     pid_t pid = fork(); // Creates a new process
     if (pid < 0) {
         perror("Fork Fail\n");
@@ -39,6 +87,29 @@ void execute (char **argv) {
                 dup2(fd, STDOUT_FILENO);
                 close(fd);
 
+                argv[i] = NULL;
+                break;
+            }
+            if (strcmp(argv[i], "<") == 0) {
+                int fd = open(argv[i+1], O_RDONLY);
+                if (fd < 0) {
+                    perror("Open failed");
+                    exit(1);
+                }
+                dup2(fd, STDIN_FILENO);
+                close(fd);
+                argv[i] = NULL;
+                break;
+            }
+
+            if (strcmp(argv[i], ">>") == 0) {
+                int fd = open(argv[i+1], O_WRONLY | O_CREAT |O_APPEND, 0644);
+                if (fd < 0) {
+                    perror("Open failed");
+                    exit(1);
+                }
+                dup2(fd, STDOUT_FILENO);
+                close(fd);
                 argv[i] = NULL;
                 break;
             }
